2748d6a2346be36d1ffbd94342a3c888dfd42845
Igor Lopes <igor@nucleus.eti.br>
2017-03-27 10:43:42 -0300

    Add private messages

--- app.js
+++ app.js
@@ -22,15 +22,21 @@ var server = net.createServer(function(socket) {
     // Listener para o evento data(Quando dados são enviados pelo cliente este envento ocorre): https://nodejs.org/api/net.html#net_event_data
     socket.on('data', function(data) {
 
+        // Caso o cliente digite somente '-list', os clientes conectados serão listados
         if (data.toString().replace(/\r?\n$/, '') === '-list') {
             listClients();
+        }
+        // Caso a mensagem termine com o argumento '--p', será enviado mensagem privada para o host especificado
+        else if (data.toString().replace(/\r?\n$/, '').includes('--p')) {
+            sendPrivateMessage(data, socket);
         } else {
             // Chama função para que occorra o envio das mensagens para os demais clientes
-            sendMessages(socket.name + " disse: " + data, socket);
+            sendMessages(data, socket);
         }
     });
 
-    function sendMessages(message, sender) {
+    function sendMessages(data, sender) {
+        var message = socket.name + " disse: " + data.toString();
 
         clients.forEach(function(client) {
             // Não envia mensagem para o próprio cliente que a enviou
@@ -42,21 +48,29 @@ var server = net.createServer(function(socket) {
         console.log(message);
     }
 
+    function sendPrivateMessage(data, sender) {
+        //Filtra destinatário da string enviada pelo cliente
+        var recipient = data.toString().split('--p')[1].replace(/\s+/g, '');
+        // Filtra a mensagem sem o argumento e destinatário
+        var message = "(Mensagem Privada)" + socket.name + " disse: " + data.toString().split('--p')[0];
+        clients.forEach(function(client) {
+            if (client.name === recipient && client != sender) {
+                client.write(message + "\n");
+                return;
+            }
+        });
+    }
+
     function listClients() {
+        //Lista todos os clientes conectados
         socket.write('Clientes conectados:\n');
         socket.write('-----------------------\n');
         clients.forEach(function(client) {
             socket.write(client.name + '\n');
         });
-        //socket.write(clients.length.toString());
         socket.write('-----------------------\n');
     }
 
-  /*  function sendPrivateMessage(message, client) {
-
-    }
-    */
-    
 }).listen(port, () => {
     console.log("Servidor escutando na porta " + port);
-})
+});
