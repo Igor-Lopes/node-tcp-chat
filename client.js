51334b0551fd90b9d8e5a4e3a90e1f0b8dda8aeb
Igor Lopes <igor@nucleus.eti.br>
2017-05-30 00:03:26 -0300

    Add RSA

--- client.js
+++ client.js
@@ -1,12 +1,26 @@
 var net = require('net');
+var crypto = require('crypto');
+var rsaKeygen = require('rsa-keygen');
+
+var serverPublicKey = '';
+
+var keys = rsaKeygen.generate();
 
 var client = new net.Socket();
 client.connect(5000, '127.0.0.1', function() {
     console.log('Connectado no chat');
+    client.write(keys.public_key);
 });
 
 client.on('data', function(data) {
-    console.log(data.toString());
+    if (data.includes('PUBLIC KEY')) {
+        serverPublicKey = data.toString();
+    } else {
+        var decrypted = crypto.privateDecrypt({
+            key: keys.private_key
+        }, data);
+        console.log(decrypted.toString());
+    }
 });
 
 client.on('close', function() {
@@ -17,5 +31,8 @@ client.on('close', function() {
 var stdin = process.openStdin();
 
 stdin.addListener("data", function(data) {
-    client.write(data.toString());
+    var result = crypto.publicEncrypt({
+        key: serverPublicKey
+    }, new Buffer(data.toString()));
+    client.write(result);
 });
--- server.js
+++ server.js
@@ -6,6 +6,12 @@ Chat TCP Simples com NodeJS
 
 // Módulo Net, nativo do Node, para criar servidor e cliente TCP: https://nodejs.org/api/net.html
 var net = require('net');
+
+var crypto = require('crypto');
+var rsaKeygen = require('rsa-keygen');
+
+var keys = rsaKeygen.generate();
+
 // Porta utilizada pelo servidor
 var port = 5000;
 // Array para armazenar sockets dos clientes que se conectam
@@ -15,10 +21,11 @@ var clients = [];
 var server = net.createServer(function(socket) {
     // Obter IP e porta do cliente que se conecta
     socket.name = socket.remoteAddress + ":" + socket.remotePort;
-    // Escrever mensagem de boas-vindas para cliente que se conecta
-    socket.write("Bem-vindo " + socket.name + "\n");
+    //Envia Public Key
+    socket.write(keys.public_key);
     // Insere socket do cliente no array de clientes
     clients.push(socket);
+
     // Listener para o evento data(Quando dados são enviados pelo cliente este envento ocorre): https://nodejs.org/api/net.html#net_event_data
     socket.on('data', function(data) {
 
@@ -27,16 +34,26 @@ var server = net.createServer(function(socket) {
             listClients();
         }
         // Caso a mensagem termine com o argumento '--p', será enviado mensagem privada para o host especificado
-        else if (data.toString().replace(/\r?\n$/, '').includes('--p')) {
+        else if (data.includes('PUBLIC KEY')) {
+            clients.forEach(function(client) {
+                // Não envia mensagem para o próprio cliente que a enviou
+                if (client == socket) {
+                    socket.key = data.toString();
+                }
+            });
+        } else if (data.toString().replace(/\r?\n$/, '').includes('--p')) {
             sendPrivateMessage(data, socket);
         } else {
             // Chama função para que occorra o envio das mensagens para os demais clientes
-            sendMessages(data, socket);
+            //  sendMessages(data, socket);
         }
     });
 
     function sendMessages(data, sender) {
-        var message = socket.name + " disse: " + data.toString();
+        var decrypted = crypto.privateDecrypt({
+            key: keys.private_key
+        }, data);
+        var message = socket.name + " disse: " + decrypted.toString();
 
         clients.forEach(function(client) {
             // Não envia mensagem para o próprio cliente que a enviou
@@ -50,12 +67,18 @@ var server = net.createServer(function(socket) {
 
     function sendPrivateMessage(data, sender) {
         //Filtra destinatário da string enviada pelo cliente
+        var decrypted = crypto.privateDecrypt({
+            key: keys.private_key
+        }, data);
         var recipient = data.toString().split('--p')[1].replace(/\s+/g, '');
         // Filtra a mensagem sem o argumento e destinatário
-        var message = "(Mensagem Privada)" + socket.name + " disse: " + data.toString().split('--p')[0];
+        var message = "(Mensagem Privada)" + socket.name + " disse: " + decrypted.toString().split('--p')[0];
         clients.forEach(function(client) {
             if (client.name === recipient && client != sender) {
-                client.write(message + "\n");
+                var encrypted = crypto.publicEncrypt({
+                    key: client.key
+                }, new Buffer(message));
+                client.write(encrypted + "\n");
                 return;
             }
         });
